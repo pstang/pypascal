6741fda19fc172b2e4a1c5b95dc2d78943555280
Pascal Stang <pstang@procyonengineering.com>
2022-05-15 18:52:59 -0700

    influxSender: General bux fix and convert to using logger

--- influxSender/influxSender.py
+++ influxSender/influxSender.py
@@ -1,55 +1,93 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+"""
+InfluxDB Sender helper library.
+Copyright (c) 2021 Pascal Stang
+This program comes with ABSOLUTELY NO WARRANTY.
+
+This module is an InfluxDB client implementation targeted towards sending
+structured data.
+
+The module can also be run as a console tool, but only as a test.
+
+TODO(pstang): Refine API to be more object-oriented.
+
+Example Usage:
+
+  import influxSender
+
+  # Create sender.
+  db = InfluxSender(loglevel=logging.DEBUG)
+  # Open database client.
+  db.open(host='localhost', port=8086, database='demo')
+  # Sent data.
+  kvdict = {'myvalue1': 123, 'myvalue2': 456}
+  db.sendDict(measurement='SenderTest', tags=None, recorddict=kvdict, time=None)
+  # Done.
+  db.close()
+"""
+
+# system
 import datetime
+import logging
+import sys
+# package
 from influxdb import InfluxDBClient
 
 class InfluxSender:
-  def __init__(self, debugflag=False):
+  def __init__(self, loglevel=logging.ERROR):
+    """Create and initialize InfluxSender object."""
     # Initialize state.
-    self._DEBUG = debugflag
     self.database = ''
-
-  def debugPrint(self, s):
-    if self._DEBUG:
-      print("InfluxSender: " + s)
-    return
+    logging.basicConfig(format = '%(levelname)s:%(name)s:%(message)s', level=loglevel)
+    self._log = logging.getLogger(__name__)
 
   def open(self, host, port, database):
-    self.debugPrint("Initializing InfluxDB client: Host={} Port={} Database=\'{}\'".format(host, port, database))
+    """Open access to the database."""
+    self._log.info("Open Host={:s}:{:d} db=\'{:s}\'".format(host, port, database))
     # Create instance.
     #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
     self.dbclient = InfluxDBClient(host=host, port=port, database=database)
     self.database = database
 
   def close(self):
+    """Close access to the database."""
+    self._log.info("Close db=\'{:s}\'".format(self.database))
+    self.dbclient.close()
+    self.database = ''
     return
 
   def sendPoints(self, measurement, tags, linestring, time=None):
+    """Send a set of data points to database (data in linestring 'key1=value1,key2=value2' format)."""
     # Formulate influxDB line protocol.
     tagstring = InfluxSender.dict2lineformat(tags, string_use_quotes=False)
+    timestamp_ns = InfluxSender.timestamp_ns(time)
     if tagstring is None:
-      dbstring = "{:s},{:s} {:s} {:d}".format(measurement, tagstring, linestring, int(InfluxSender.timestamp(time)*1e9))
+      dbstring = "{:s} {:s} {:d}".format(measurement, linestring, timestamp_ns)
     else:
-      dbstring = "{:s} {:s} {:d}".format(measurement, linestring, int(InfluxSender.timestamp(time)*1e9))
-    self.debugPrint(dbstring)
-    # Send it.
+      dbstring = "{:s},{:s} {:s} {:d}".format(measurement, tagstring, linestring, timestamp_ns)
+    self._log.debug("Sending: " + dbstring)
+    # Send it to the database.
     return self.dbclient.write_points(dbstring, database=self.database, protocol=u'line')
 
   def sendDict(self, measurement, tags, recorddict, time=None):
-    #print(recorddict)
-    #print(InfluxSender.dict2lineformat(recorddict))
+    """Send a set of data points to database (data in dict key:value format)."""
     return self.sendPoints(measurement=measurement, tags=tags, linestring=InfluxSender.dict2lineformat(recorddict), time=time)
 
-  def timestamp(t):
-    # Generate influxdb timestamp from datetime object.
+  def timestamp(t=None):
+    """Generate an influxdb timestamp from datetime object (if None, timestamp is generated for now)."""
     if t == None:
       t = datetime.datetime.utcnow()
     return (t - datetime.datetime.utcfromtimestamp(0)).total_seconds()
 
+  def timestamp_ns(t=None):
+    """Generate an influxdb nanoseconds timestamp from datetime object (if None, timestamp is generated for now)."""
+    return int(InfluxSender.timestamp(t)*1e9)
+
   def dict2lineformat(d, string_use_quotes=True):
     report = ""
     if d is None:
-      return report
+      return None
     for key in sorted(d):
       if isinstance(d[key], float):
         report = report + "{:s}={:0.7f},".format(key, d[key])
@@ -71,8 +109,11 @@ class InfluxSender:
 
 def main(argv):
   # test operation
-  print("Initialize object")
-  ifs = InfluxSender(debugflag = True)
+  db = InfluxSender(loglevel=logging.DEBUG)
+  db.open(host='localhost', port=8086, database='demo')
+  kvdict = {'myvalue1': 123, 'myvalue2': 456}
+  db.sendDict(measurement='SenderTest', tags=None, recorddict=kvdict, time=None)
+  db.close()
 
 if __name__ == "__main__":
   main(sys.argv)
--- influxSender/influxSender_demo.py
+++ influxSender/influxSender_demo.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
+"""
+InfluxSender library demonstration.
+Copyright (c) 2021 Pascal Stang
+This program comes with ABSOLUTELY NO WARRANTY.
+"""
+
 # system
+import logging
 import datetime
 import random
 import sys
 import time
-# internal
+# package
 import influxSender
 
 # Configuration options.
 Config = {
   'Influx': {
     # Host/Database options.
-    #'Host': '192.168.2.40',
     'Host': 'localhost',
     'Port': 8086,
     'Database': 'demo',
     # Record options.
     'TimeStamp': 'host',
-    # Debug.
-    'Debug': True,
+    # Logging options.
+    'LogLevel': logging.INFO,
   },
 }
 
@@ -27,14 +33,14 @@ def main(argv):
   global Config
   global db
 
-  print("InfluxDB demo")
-  print("-------------")
+  print("InfluxSender demo")
+  print("-----------------")
 
   # Influx database connection
   if Config['Influx']['Host']:
     print("Initializing InfluxDB Sender")
     # Create instance.
-    db = influxSender.InfluxSender(debugflag=Config['Influx']['Debug'])
+    db = influxSender.InfluxSender(loglevel=Config['Influx']['LogLevel'])
     db.open(host=Config['Influx']['Host'], port=Config['Influx']['Port'], database=Config['Influx']['Database'])
   else:
     db = None
@@ -42,13 +48,13 @@ def main(argv):
   # Send some stuff
   print("Sending data to database")
   for n in range(10):
-    # Create a dict of key:values to send.
+    # Create a dict of key:value pairs to send.
     # (this allows us to record several measurement values at once)
-    valueDict = {'loopNum': n, 'setpoint': 50+n, 'temperature': random.random()}
+    valueDict = {'loopNum': n, 'rampdata': 50+n, 'randomdata': random.random()}
     print(valueDict)
     # Send the data to the database.
     # If passing time=None, the timestamp is automatically recorded as "now".
-    db.sendDict(measurement='thermalchamber', tags=None, recorddict=valueDict, time=None)
+    db.sendDict(measurement='influxSender', tags=None, recorddict=valueDict, time=None)
     # Wait a moment to simulate passage of real time between measurements
     time.sleep(1)
 
